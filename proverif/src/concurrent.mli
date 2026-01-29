86540e22f893d4b033522df04348830e2c19d417
Jushka <atlassian@julia.bellardo.com>
2026-01-29 15:11:57 +0000

    meeting2

--- proverif/src/concurrent.mli
+++ proverif/src/concurrent.mli
@@ -6,4 +6,3 @@ val check_token : token -> (token -> bool) -> bool
 
 val list_exists : (token -> 'a -> bool) -> 'a list -> (*int ->*) bool
 val bool_function_list_or : (token -> bool) list -> (*int ->*) bool
-
